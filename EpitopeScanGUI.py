80e30b31f9d59d30040cb828353ea3328d87b470
Aleks Kovalenko <ikovalenko239@gmail.com>
2023-04-02 20:00:20 +0100

    Developed EpitopeScanGUI

---
+++ EpitopeScanGUI.py
@@ -0,0 +1,249 @@
+#! /usr/bin/env python
+import os
+import sys
+import pandas as pd
+import streamlit as st
+from datetime import datetime, timedelta 
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__)))
+from utils.GUIutils import *
+from utils.ProteinUtils import *
+
+def main():
+ 
+	# Headline
+	st.markdown("""
+	### EpitopeScan output analysis
+
+- This app allows to explore `EpitopeScan` outputs
+
+- App built with `Python`, `Streamlit` and `Plotly`
+---""")
+
+	# Sidebar for input
+	with st.sidebar.header('Input data'):
+	
+		st.sidebar.markdown("""
+		Provide EpitopeScan outputs for analysis:
+		""")
+
+		# input files for analysis
+		uploaded_files = st.sidebar.file_uploader("Upload 3 input files for epitope",
+												  accept_multiple_files=True)
+
+		# verify inputs
+		verified = False
+		if len(uploaded_files) > 0:
+			verified = VerifyInput(uploaded_files)
+
+	# proceed if input files are verified
+	if verified:
+
+		# load data from files
+		with st.spinner(f"Loading input tables..."):
+			peptide, mutation_data, AA_mutation_matrix, NA_mutation_matrix = LoadData(uploaded_files)
+
+		## Print key sample statistics ##
+		KeyStatisticsSection(peptide, mutation_data)
+
+		## Plot key statistics in time ##
+		KeyStatisticsPlotSection(mutation_data)
+
+		## List and plot found mutations ##
+		MutationStatSection(peptide, mutation_data)
+
+		## Sequence conservation ##
+		SequenceConservationSection(NA_mutation_matrix, AA_mutation_matrix)
+
+		## Lineage statistics ##
+		LineageStatSection(peptide, mutation_data)
+
+def KeyStatisticsSection(peptide, mutation_data):
+	st.write('<p style="font-size:22px;weight:bold">1. Key sample statistics</p>',
+			 unsafe_allow_html=True)
+
+	# load reference genome
+	script_dir = os.path.realpath(os.path.dirname(__file__))
+	with open(f'{script_dir}/reference_sequences/EPI_ISL_402124.fasta', 'r') as f:
+		lines = f.readlines()
+	reference_genome = lines[1].strip()
+	del lines
+
+	## load reference proteome
+	proteome = ReadProteinsFromFile(f'{script_dir}/reference_sequences/protein_sequences_reference.fasta')
+
+	# map peptide onto proteome and assign coding DNA sequence
+	peptide = MapPeptides([peptide],
+	                      proteome,
+	                      reference_genome,
+	                      verbose=False)[0]
+
+	# print peptide info
+	st.write(f"Peptide {peptide.name}, sequence: **{peptide.sequence}** ({len(peptide)} residues)")
+	part1 = f"Originates from {peptide.parent_protein[0]} protein (residues {peptide.protein_start} to {peptide.protein_start + len(peptide) - 1})."
+	part2 = f" Genome coordinates: {peptide.genome_start}-{peptide.genome_end}"
+	st.write(part1 + part2)
+
+	with st.container():
+		# create two column layout
+		col1, col2 = st.columns(2)
+
+		# first column is to print statistics
+		with col1:
+			# whether to stat samples without metadata
+			stat_only_metadata = st.checkbox('Only stat samples with metadata')
+		
+			key_stat = PrintKeyStat(peptide.name, mutation_data,
+				                    stat_only_metadata)
+
+		# second column to plot pie chart
+		with col2:
+			pie_chart = KeyPieChart(key_stat)
+			st.plotly_chart(pie_chart)
+
+def KeyStatisticsPlotSection(mutation_data):
+
+	st.write('<p style="font-size:22px;weight:bold">2. Key sample statistics in time</p>',
+			 unsafe_allow_html=True)
+
+	# plot counts or proportion
+	col1, col2 = st.columns((1, 3))
+	with col1:
+		plot_as2 = st.selectbox('Plot as:', ('counts', 'proportion'), key=1)
+		plot_proportion2 = True if plot_as2 == 'proportion' else False
+
+	with st.spinner(f"Generating weekly stat plot..."):
+		key_stat_plot = PlotKeyStat(mutation_data,
+									plot_proportion=plot_proportion2)
+		st.plotly_chart(key_stat_plot)
+
+def MutationStatSection(peptide, mutation_data):
+	st.write('<p style="font-size:22px;weight:bold">3. Stat mutations</p>',
+			 unsafe_allow_html=True)
+
+	# whether to stat samples without metadata
+	stat_only_metadata = st.checkbox('Only stat samples with metadata',
+									  value=True)
+	
+	# additional parameters
+	with st.container():
+		col1, col2, col3, col4, col5 = st.columns((1.5, 1.5, 1.2, 1.2, 1))
+
+		with col1:
+			stat_as = st.selectbox('Stat mutations as', ('individual', 'combinations'), key=2)
+		with col2:
+			blosum_v = st.text_input('BLOSUM score version', '90')
+			blosum_v = int(blosum_v)
+		with col3:
+			def_start_date = mutation_data['sample_date'].min()
+			start_date = st.date_input("Stat starting from:", def_start_date, key='d1',
+										disabled=not stat_only_metadata)
+			start_date = datetime(start_date.year, start_date.month, start_date.day)
+		with col4:
+			def_end_date = mutation_data['sample_date'].max()
+			end_date = st.date_input("and up to:", def_end_date, key='d2',
+									 disabled=not stat_only_metadata)
+			end_date = datetime(end_date.year, end_date.month, end_date.day)
+		with col5:
+			plot_as3 = st.selectbox('Plot as:', ('counts', 'proportion'), key=3,
+									disabled=not stat_only_metadata)
+			plot_proportion3 = True if plot_as3 == 'proportion' else False
+
+	# generate and display stats
+	with st.container():
+
+		# create two column layout
+		col1, col2 = st.columns((1.7, 1))
+
+		# print table mutation summary
+		with st.spinner(f"Generating mutation summary..."):
+
+			# filter mutation data by metadata and dates
+			if stat_only_metadata:
+				filtered_mutation_data = mutation_data[mutation_data['has_metadata'] == 1]
+				filtered_mutation_data = filtered_mutation_data[filtered_mutation_data['sample_date'] >= start_date]
+				filtered_mutation_data = filtered_mutation_data[filtered_mutation_data['sample_date'] <= end_date]
+			else:
+				filtered_mutation_data = mutation_data
+
+			summary = MutationTextSummary(peptide,
+										  filtered_mutation_data,
+					                      stat_mutations=0,
+					                      sort_by=0,
+					                      blosum_version=blosum_v)
+			
+			with col1:
+				st.write("**Mutations summarised:**")
+				st.write(summary)
+
+		# mutation selection for plotting
+		if stat_only_metadata:
+				
+				# select mutations to plot
+				with col2:
+					found_mutations = summary.iloc[:, 1].values
+					top_found_mutations = found_mutations[:5] if len(found_mutations)>5 else found_mutations
+					selected_mutations = st.multiselect('Mutations to plot:',
+														found_mutations,
+														top_found_mutations)
+	# plot selected mutations in time
+	if stat_only_metadata:
+		with st.spinner(f"Generating timeline plot..."):
+			# generate plot
+			muts_time_plot = PlotMutationStat(mutation_data[mutation_data['has_metadata'] == 1],
+						                      selected_mutations,
+											  plot_proportion=plot_proportion3,
+											  time_start=start_date,
+											  time_end=end_date)
+			
+			st.plotly_chart(muts_time_plot, use_container_width=True)
+
+@st.cache_data
+def SequenceConservationSection(NA_mutation_matrix, AA_mutation_matrix):
+	with st.spinner(f"Generating sequence conservation plot..."):
+		sequence_conservation_plot = PlotSeqConservation(NA_mutation_matrix,
+														 AA_mutation_matrix)
+		st.plotly_chart(sequence_conservation_plot, use_container_width=True)
+
+def LineageStatSection(peptide, mutation_data):
+	st.write('<p style="font-size:22px;weight:bold">4. Stat mutations by lineage</p>',
+			 unsafe_allow_html=True)
+
+	# specify date range for analysis
+	# three last weeks by default
+	with st.container():
+			col1, col2, col3, pad = st.columns((1.2, 1.2, 1, 1))
+
+			def_end_date = mutation_data['sample_date'].max()
+			def_start_date = def_end_date - timedelta(weeks=3) 
+
+			with col1:
+				start_date = st.date_input("Stat starting from:", def_start_date, key='d4')
+				start_date = datetime(start_date.year, start_date.month, start_date.day)
+			with col2:
+				end_date = st.date_input("and up to:", def_end_date, key='d5')
+				end_date = datetime(end_date.year, end_date.month, end_date.day)
+			with col3:
+				report_as = st.selectbox('Report samples as:', ('counts', 'proportion'), key=4)
+				report_proportion = True if report_as == 'proportion' else False
+
+	# select mutations to stat
+	with st.spinner(f"Generating lineage statistics..."):
+
+		summary2 = MutationTextSummary(peptide,
+									  mutation_data[mutation_data['has_metadata'] == 1],
+				                      stat_mutations=0,
+				                      sort_by=0,
+				                      blosum_version=90)
+		found_mutations2 = summary2.iloc[:, 1].values
+
+		# get lineage summary
+		lineage_summary = StatLineages(mutation_data[mutation_data['has_metadata'] == 1],
+						               mutations=found_mutations2,
+						               report_proportion=report_proportion,
+						               time_start=start_date,
+						               time_end=end_date)
+		st.write(lineage_summary)
+
+if __name__ == "__main__":
+	main()
---
+++ utils/GUIutils.py
@@ -0,0 +1,507 @@
+import sys
+import os
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__)))
+from Stat import *
+from ProteinUtils import *
+
+def VerifyInput(files):
+	"""
+	Verify input files for EpitopeScanGUI
+
+	files - streamlit file_uploader
+	
+	Returns:
+	right_input - bool, True if inputs are right
+	""" 
+
+	right_input = False
+
+	if len(files) > 3: 
+		st.sidebar.write(f"Expected 3 files, but {len(files)} provided. Delete extra")
+	elif len(files) < 3: 
+		st.sidebar.write(f"Expected 3 files, but {len(files)} provided. Upload missing")
+	else:
+		# get epitope neames from files
+		file_names = [files[i].name for i in range(len(files))]
+		names = set([name.split('_')[0] for name in file_names])
+		if len(names) > 1:
+			st.sidebar.write(f"Input files seemingly belong to different epitopes. Revise input")
+		else:
+			name = names.pop()
+			if not f"{name}_AA_mutation_matrix.csv" in file_names:
+				st.sidebar.write(f"Missing {name}_AA_mutation_matrix.csv")
+			elif not f"{name}_NA_mutation_matrix.csv" in file_names:
+				st.sidebar.write(f"Missing {name}_NA_mutation_matrix.csv")
+			elif not f"{name}_mutation_data.tsv" in file_names:
+				st.sidebar.write(f"Missing {name}_mutation_data.tsv")
+			else:
+				right_input = True
+
+	return right_input
+
+@st.cache_data
+def LoadData(files):
+	"""
+	Load EpitopeScan outputs into DataFrames
+
+	files - streamli file_uploader
+
+	Returns:
+	peptide_name, mutation_data, AA_mutation_matrix, NA_mutation_matrix
+	"""
+	
+	# get peptide name
+	peptide_name = files[0].name.split('_')[0]
+
+	for i in range(len(files)):
+
+		if files[i].name == f"{peptide_name}_AA_mutation_matrix.csv":
+			AA_mutation_matrix = pd.read_csv(files[i], index_col=0)
+			peptide = Protein(peptide_name, ''.join(AA_mutation_matrix.index)) 
+
+		elif files[i].name == f"{peptide_name}_NA_mutation_matrix.csv":
+			NA_mutation_matrix = pd.read_csv(files[i], index_col=0)
+
+		elif files[i].name == f"{peptide_name}_mutation_data.tsv":
+			mutation_data = pd.read_csv(files[i], sep='\t', index_col=0)
+			mutation_data['sample_date']= pd.to_datetime(mutation_data['sample_date'],
+														 format='%Y-%m-%d')
+
+	return peptide, mutation_data, AA_mutation_matrix, NA_mutation_matrix
+
+@st.cache_data
+def PrintKeyStat(peptide_name, mutation_data, stat_only_metadata):
+	"""
+	Return and print key statistics of loaded samples
+
+	peptide_name - str, peptide name
+	mutation_data - pd DataFrame, data to stat
+	stat_only_metadata - bool, stat only samples with metadata
+
+	Returns
+	key_stat - dict, key samples statistics
+	"""
+	total = mutation_data.shape[0]
+	has_metadata = sum(mutation_data['has_metadata'] == 1)
+
+	mask = mutation_data['has_metadata'] == 1 if stat_only_metadata \
+		   else mutation_data['has_metadata'].isin([0, 1])
+
+	no_mutation = sum((mutation_data['AA_mutations'].isna()) & mask)
+	no_coverage = sum((mutation_data['AA_mutations'] == '-') & mask)
+	non_functional = sum((mutation_data['AA_mutations'] == 'NF') & mask)
+	
+	have_mutation = has_metadata if stat_only_metadata else total
+	have_mutation = have_mutation - no_mutation - no_coverage - non_functional
+
+	start_date = mutation_data['sample_date'].min().strftime('%d/%m/%Y')
+	end_date = mutation_data['sample_date'].min().strftime('%d/%m/%Y')
+
+	# create dictionary
+	key_stat = {'Have mutation' : have_mutation,
+				'No mutation' :  no_mutation,
+				'No coverage' : no_coverage,
+				'non-functional' : non_functional}
+
+	# print statistics
+	st.write(f"**Data for peptide {peptide_name} contains:**")
+	st.write(f"{total} samples")
+	st.write(f"of them {has_metadata} have metadata ({round(has_metadata*100/total, 1)}% of all)")
+	st.write(f"dated between {start_date} and {end_date}")
+
+	if stat_only_metadata:
+		st.write("**Of samples with metadata:**")
+	else:
+		st.write("**Of all samples:**")
+	st.markdown(f"- {have_mutation} ({round(have_mutation * 100/ total, 2)}%) have mutations")
+	st.markdown(f"- {no_mutation} ({round(no_mutation * 100/ total, 2)}%) have no mutations")
+	st.markdown(f"- {no_coverage} ({round(no_coverage * 100/ total, 2)}%) reported with insufficient coverage")
+	st.markdown(f"- {non_functional} ({round(non_functional * 100/ total, 2)}%) reported as non-functional")
+	
+	return key_stat
+
+@st.cache_data
+def KeyPieChart(key_stat):
+	"""
+	Assemble pie chart of key sample statistics
+
+	key_stat - dict, key samples statistics
+
+	Returns:
+	fig - go.Figure, assembled figure
+	"""
+	# create figure
+	fig = go.Figure()
+
+	fig.add_trace(go.Pie(labels=list(key_stat.keys()),
+			        	 values=list(key_stat.values()))
+				 )
+
+	fig.update_traces(textfont_size=20)
+	fig.update_layout(title_text = "Samples breakdown",
+					  title_x = 0.5,
+					  title_font_size=20,
+					  legend_font_size=15,
+					  height=500, width=500)
+
+	return fig
+
+@st.cache_data
+def KeyWeeklyStat(df, report_proportion=False):
+	"""
+	Calculate key statistics per week
+
+	df - pd.DataFrame, mutation data to aggregate
+	report_proportion - bool, report proportion instead of counts
+	
+	Returns:
+	stat - pd.DataFrame, key statistics per week
+	"""
+
+	# create dummy week column
+	max_week = df['epi_week'].max()
+	min_week = df['epi_week'].min()
+	week_column = pd.DataFrame()
+	week_column['epi_week'] = [w for w in range(min_week, max_week + 1)]
+
+	# get statistics for each type of reports
+
+	# non-functional
+	grouped_nf = df[df['AA_mutations'] == "NF"] \
+	             .groupby('epi_week', as_index=False) \
+	             .count()[["epi_week", 'sequence_name']] \
+	             .rename(columns={'sequence_name':'Non-functional'})
+
+	# no coverage
+	grouped_nc = df[df['AA_mutations'] == "-"] \
+	             .groupby('epi_week', as_index=False) \
+	             .count()[["epi_week", 'sequence_name']] \
+	             .rename(columns={'sequence_name':'No coverage'})
+
+	# no mutations
+	grouped_nm = df[df['AA_mutations'].isna()] \
+	             .groupby('epi_week', as_index=False) \
+	             .count()[["epi_week", 'sequence_name']] \
+	             .rename(columns={'sequence_name':'No mutations'})
+
+	# with mutations
+	grouped_mu = df[(df['AA_mutations'] != "-")  & \
+	             (df['AA_mutations'] != "NF") & \
+	             (~df['AA_mutations'].isna())] \
+	             .groupby('epi_week', as_index=False) \
+	             .count()[["epi_week", 'sequence_name']] \
+	             .rename(columns={'sequence_name':'With mutations'})
+
+	# total samples
+	grouped_to = df.groupby('epi_week', as_index=False) \
+	             .count()[["epi_week", 'sequence_name']] \
+	             .rename(columns={'sequence_name':'Total samples'})
+
+	# merge all DataFrames
+	stat = week_column.merge(grouped_nf, on='epi_week', how='left') \
+	       .merge(grouped_nc, on='epi_week', how='left') \
+	       .merge(grouped_nm, on='epi_week', how='left') \
+	       .merge(grouped_mu, on='epi_week', how='left') \
+	       .merge(grouped_to, on='epi_week', how='left') \
+
+	stat = stat.fillna(0).astype(int)
+
+	if report_proportion:
+	    for col in stat.columns[1:-1]:
+	        stat[col] = stat[col] / stat['Total samples']
+	    stat.drop(columns=['Total samples'], inplace=True)
+
+	return stat
+
+@st.cache_data
+def PlotKeyStat(mutation_data, plot_proportion=False):
+	"""
+	Assemble bar chart of key sample statistics in time
+
+	mutation_data - pd DataFrame, mutation data for plotting
+	plot_proportion - bool, plot as proportion of all samples
+
+	Returns:
+	fig - go.Figure, assembled figure
+	"""
+	mutation_data = mutation_data[mutation_data['has_metadata'] == 1]
+	mutation_data['epi_week'] = mutation_data['epi_week'].astype('int32')
+
+	# get statistics
+	stat = KeyWeeklyStat(mutation_data, plot_proportion)
+
+	# create figure
+	fig = go.Figure()
+
+	# bar plots for each statistic
+	for value in stat.columns[1:]:
+	    fig.add_trace(go.Bar(x=stat['epi_week'],
+							 y=stat[value],
+							 name=value)
+					 )
+
+	fig.update_layout(title_text = "Samples statistics per week",
+					  title_x = 0.5,
+					  xaxis_title = "Pandemic week",
+					  height=500, width=800)
+
+	if plot_proportion:
+	    fig.update_layout(yaxis_title = "Proportion in total samples per week")
+	else:
+	    fig.update_layout(yaxis_title = "Count")
+	    
+	return fig
+
+@st.cache_data
+def StatMutations(df,
+                  mutations,
+                  report_proportion=False,
+                  time_start=None,
+                  time_end=None):
+	"""
+	Calculate statistics per mutation
+
+	df - pd DataFrame, mutation data to stat
+	mutations - list(str), list of mutations to stat
+	report_proportion - bool, report proportion instead of counts
+	time_start - datetime, start date to filter
+	time_end - datetime, end time to filter
+
+	Returns:
+	stat_df - pd DataFrame, calculated statistic per mutation
+	"""
+
+    df['epi_week'] = df['epi_week'].astype('int32')
+    
+    # filter by date if necessary
+    if not time_start is None:
+        df = df[df['sample_date'] >= time_start]
+    if not time_end is None:
+        df = df[df['sample_date'] <= time_end]
+        
+    # create dummy week column
+    max_week = df['epi_week'].max()
+    min_week = df['epi_week'].min()
+    week_column = pd.DataFrame()
+    week_column['epi_week'] = [w for w in range(min_week, max_week + 1)]
+        
+    # drop samples without coverage
+    df = df[df['AA_mutations'] != "-"]
+    
+    # count samples per week
+    sample_counts = df.groupby('epi_week', as_index=False) \
+                    .count()[["epi_week", 'sequence_name']] \
+                    .rename(columns={'sequence_name': 'n_samples'})
+    
+    
+    # get statistics for all mutations
+    counts_per_mutation = []
+    for mut in mutations:
+        
+        muts = mut.split(',')
+        
+        # subset samples with target mutation
+        mask = ~df['sequence_name'].isna()
+        for m in muts:
+            mask = mask & (df[m] == 1)
+
+        # impose additional filtering for exact combination
+        if len(muts) > 1:
+            unwanted_muts = set(df.columns[3:-4]) - set(muts)
+            for m in unwanted_muts:
+                   mask = mask & (df[m] == 0)
+
+        # count samples with target mutations per week
+        mutation_counts = df[mask].groupby('epi_week', as_index=False) \
+                          .count()[['epi_week', 'sequence_name']] \
+                          .rename(columns={'sequence_name': mut})
+        
+        counts_per_mutation.append(mutation_counts)
+    
+    # merge all DataFrames
+    stat_df = week_column.merge(sample_counts, on='epi_week', how='left')
+    for counts in counts_per_mutation:
+        stat_df = stat_df.merge(counts, on='epi_week', how='left')
+    
+    stat_df = stat_df.fillna(0).astype(int)
+    
+    if report_proportion:
+        for col in stat_df.columns[2:]:
+            stat_df[col] = stat_df[col] / stat_df['n_samples']
+        stat_df.drop(columns=['n_samples'], inplace=True)
+    
+    return stat_df
+
+@st.cache_data
+def PlotMutationStat(df,
+                     mutations,
+                     plot_proportion=False,
+                     time_start=None, time_end=None):
+	"""
+	Assemble bar chart of mutation statistics per week
+
+	df - pd DataFrame, mutation data to stat
+	mutations - list(str), list of mutations to stat
+	report_proportion - bool, report proportion instead of counts
+	time_start - datetime, start date to filter
+	time_end - datetime, end time to filter
+
+	Returns:
+	fig - go.Figure, assembled figure
+	"""
+    
+    # get statistics
+    stat = StatMutations(df, mutations, plot_proportion)
+
+    # create figure
+    fig = go.Figure()
+
+    # bar plot for mutation statistic
+    i = 1 if plot_proportion else 2
+    for col in stat.columns[i:]:
+        fig.add_trace(go.Bar(x=stat['epi_week'],
+                             y=stat[col],
+                             name=col))
+    
+    fig.update_layout(title_text = f"Mutations statistics per week",
+                      title_x = 0.5,
+                      xaxis_title = "Pandemic week")
+
+    if plot_proportion:
+        fig.update_layout(yaxis_title = "Proportion in samples with coverage")
+    else:
+        fig.add_trace(go.Bar(x=stat['epi_week'],
+                         y=stat['n_samples'],
+                         name='Total samples'))
+        fig.update_layout(yaxis_title = "Count")
+
+    return fig
+
+@st.cache_data
+def PlotSeqConservation(NA_matrix, AA_matrix):
+	"""
+	Assemble bar chart of overall AA and NA sequence conservation
+
+	NA_matrix - pd DataFrame, NA substitution count matrix
+	AA_matrix - pd DataFrame, AA substitution count matrix
+
+	Returns:
+	fig - go.Figure, assembled figure
+	"""
+    
+    # calculate conservation
+    NA_matrix = NA_matrix.apply(lambda row: row/row.sum(), axis=1)
+    AA_matrix['Conservation'] = AA_matrix.apply(lambda row: row[row.name]/row.sum(), axis=1)
+    
+    # plot conservation
+    fig = make_subplots(rows=2, cols=1,
+                        y_title = "Conservation",
+                        x_title = "Sequence",
+                        vertical_spacing=0.08)
+
+    # AA conservation
+    fig.append_trace(go.Bar(x = [i for i in range(len(AA_matrix.index))],
+                            y = AA_matrix['Conservation'],
+                            name="Protein sequence"),
+                     row=1, col=1)
+
+    fig.update_xaxes(tickmode = 'array',
+                     tickvals = [i for i in range(len(AA_matrix.index))],
+                     ticktext = AA_matrix.index.values,
+                     row=1, col=1)
+
+    # NA conservation
+    for b in 'ATCGΔ':
+        fig.append_trace(go.Bar(x = [i for i in range(len(NA_matrix.index))] ,
+                                y = NA_matrix[b],
+                                name = b),
+                         row=2, col=1)
+
+    fig.update_xaxes(tickmode = 'array',
+                     tickvals = [i for i in range(len(NA_matrix.index))],
+                     ticktext = NA_matrix.index.values,
+                     row=2, col=1)
+
+    fig.update_layout(title_text="Sequence conservation",
+                      title_x=0.5,
+                      showlegend=False,
+                      barmode='stack',
+                      height=500, width=800)
+
+    return fig
+
+@st.cache_data
+def StatLineages(df,
+                 mutations,
+                 report_proportion,
+                 time_start=None, time_end=None):
+	"""
+	Aggreagate samples data by lineage and mutations
+
+	df - pd DataFrame, mutation data to stat
+	mutations - list(str), list of mutations to stat
+	report_proportion - bool, report proportion instead of counts
+	time_start - datetime, start date to filter
+	time_end - datetime, end time to filter
+
+	Returns:
+	lineage_counts - pd.DataFrame, sample data aggregated by lineage and mutation
+	"""
+    
+    # filter by date if necessary
+    if not time_start is None:
+        df = df[df['sample_date'] >= time_start]
+    if not time_end is None:
+        df = df[df['sample_date'] <= time_end]
+    
+    # count lineages present in this subset
+    lineage_counts = df.groupby('usher_lineage', as_index=False) \
+                      .count()[["usher_lineage", 'sequence_name']] \
+                      .rename(columns={'sequence_name': 'n_samples'})
+    
+
+    # get statistics for all mutations
+    counts_per_mutation = []
+    for mut in mutations:
+        
+        muts = mut.split(',')
+        
+        # subset samples with target mutation
+        mask = ~df['sequence_name'].isna()
+        for m in muts:
+            mask = mask & (df[m] == 1)
+
+        # impose additional filtering for exact combination
+        if len(muts) > 1:
+            unwanted_muts = set(df.columns[3:-4]) - set(muts)
+            for m in unwanted_muts:
+                   mask = mask & (df[m] == 0)
+
+        # count samples with target mutations
+        mutation_counts = df[mask].groupby('usher_lineage', as_index=False) \
+                          .count()[["usher_lineage", 'sequence_name']] \
+                          .rename(columns={'sequence_name': mut})
+        
+        if mutation_counts[mut].sum() > 0:
+        	counts_per_mutation.append(mutation_counts)
+    
+    # merge all DataFrames
+    for counts in counts_per_mutation:
+        lineage_counts = lineage_counts.merge(counts, on='usher_lineage', how='left')
+    
+    lineage_counts = lineage_counts.fillna(0)
+    for col in lineage_counts.columns[1:]:
+        lineage_counts[col] = lineage_counts[col].astype(int)
+    
+    if report_proportion:
+        for col in lineage_counts.columns[1:]:
+            lineage_counts[col] = lineage_counts[col] / lineage_counts['n_samples']
+        lineage_counts.drop(columns=['n_samples'], inplace=True)
+    
+    return lineage_counts
